629ef1dc384955102e359df880a99e9f98bdda65
Yanay Zabary <yanay.tsabary@gmail.com>
2025-04-10 15:26:51 +0300

    added support for internal

--- src/core/client.ts
+++ src/core/client.ts
@@ -3,21 +3,18 @@ import axios, { AxiosInstance } from "axios";
 export interface ClientConfig {
   apiKey: string;
   projectId: string;
-  baseUrl?: string;
+  isInternal?: boolean;
 }
 
 export class ReplykeHttpClient {
   instance: AxiosInstance;
 
-  constructor({
-    apiKey,
-    projectId,
-    baseUrl = `https://api.replyke.com/api/v1/${projectId}`,
-  }: ClientConfig) {
+  constructor({ apiKey, projectId, isInternal }: ClientConfig) {
     this.instance = axios.create({
-      baseURL: baseUrl,
+      baseURL: `https://api.replyke.com/api/v1/${projectId}`,
       headers: {
         Authorization: `Bearer ${apiKey}`,
+        ...(isInternal && { "X-Replyke-Internal": "true" }),
       },
     });
   }
