40b368f4c7edc6977173e9943cf4b269fcb54012
Yanay Zabary <yanay.tsabary@gmail.com>
2025-04-13 17:51:47 +0300

    added fetchManyEntities

--- src/modules/entities/index.ts
+++ src/modules/entities/index.ts
@@ -1,6 +1,13 @@
 import { createEntity } from "./createEntity";
 import { fetchEntity } from "./fetchEntity";
+import { fetchManyEntities } from "./fetchManyEntities";
 import { updateEntity } from "./updateEntity";
 import { deleteEntity } from "./deleteEntity";
 
-export { createEntity, fetchEntity, updateEntity, deleteEntity };
+export {
+  createEntity,
+  fetchEntity,
+  fetchManyEntities,
+  updateEntity,
+  deleteEntity,
+};
